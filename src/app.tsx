ab69f4cb20e108f6f8c2374e2c02adf096b63b6f
saber2pr <saber2pr@gmail.com>
2019-06-11 16:45:20 +0000

    init

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './app.less'
+
+export const App = () => {
+  return (
+    <>
+      <header className="title">app</header>
+      <main>helloworld</main>
+      <footer>footer</footer>
+    </>
+  )
+}
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const CleanCSSPlugin = require('less-plugin-clean-css')
+const path = require('path')
+
+const extractLess = new ExtractTextPlugin('style-[hash].css')
+
+const {
+  WebpackConfig,
+  templateContent
+} = require('@saber2pr/webpack-configer')
+
+module.exports = WebpackConfig({
+  mode: 'development',
+  entry: './src/index.tsx',
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx']
+  },
+  output: {
+    filename: 'bundle-[hash].js',
+    path: path.join(__dirname, 'build')
+  },
+  module: {
+    rules: [{
+        test: /\.(ts|tsx)$/,
+        use: ['ts-loader']
+      },
+      {
+        test: /\.less$/,
+        use: extractLess.extract({
+          use: [{
+              loader: 'css-loader'
+            },
+            {
+              loader: 'less-loader',
+              options: {
+                plugins: [
+                  new CleanCSSPlugin({
+                    advanced: true
+                  })
+                ]
+              }
+            }
+          ],
+          fallback: 'style-loader'
+        })
+      }
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin({
+    templateContent
+  }), extractLess]
+})
